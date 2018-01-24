23c0cdbfd2f54f83b88aa937f6d2640dfc354a85
Tim Parker-Nance <timpn@saeon.ac.za>
2018-01-24 15:50:01 +0200

    Configuration in json, closes #4

---
+++ SAEON.Identity.Service/Config/Cert.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Hosting.Internal;
+using SAEON.Logs;
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SAEON.Identity.Service.Config
+{
+    internal static class Cert
+    {
+        public static X509Certificate2 Load()
+        {
+            using (Logging.MethodCall(typeof(Cert)))
+                try
+                {
+                    //Logging.Information("RootFolder: {RootFolder}", rootFolder);
+                    //var fileName = rootFolder + "/bin/config/saeon.ac.za.pfx";
+                    var fileName = "Config/saeon.ac.za.pfx";
+                    Logging.Information($"Loading {fileName}");
+                    return new X509Certificate2(fileName, "S@E0N.Cert");
+                }
+                catch (Exception ex)
+                {
+                    Logging.Exception(ex, "Unable to load certificate");
+                    throw;
+                }
+        }
+
+    }
+}
