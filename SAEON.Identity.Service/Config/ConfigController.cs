1f648260d5c34437ad659c0810bd54f123bc67c2
andree1985 <30987911+andree1985@users.noreply.github.com>
2018-06-30 21:36:05 +0200

    Created Client management list, add and edit views. Still need to save changes after edit. #7

--- SAEON.Identity.Service/Config/ConfigController.cs
+++ SAEON.Identity.Service/Config/ConfigController.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
+using IdentityServer4.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 namespace SAEON.Identity.Service.Config
 {
@@ -39,24 +43,85 @@ namespace SAEON.Identity.Service.Config
             return View(_logic.GetClientResource(clientId));
         }
 
+        [HttpPost]
+        public ActionResult ClientResourceAdd(IdentityServer4.EntityFramework.Entities.Client client)
+        {
+            if (ModelState.IsValid)
+            {
+                // TODO: Save changes
+                return RedirectToAction("Index");
+            }
+
+            return View(client);
+        }
+
+        [HttpPost]
+        public ActionResult ClientResourceEdit(ClientViewModel client)
+        {
+            if (ModelState.IsValid)
+            {
+                // TODO:
+                //Convert back to IS Client
+                //Save changes to DB
+
+                return RedirectToAction("ClientResources");
+            }
+
+            return View(client);
+        }
+
         public ActionResult ApiResources()
         {
             return View();
         }
+    }
 
-
+    public class ClientViewModel
+    {
+        [Required]
+        public string ClientId { get; set; } = "";
+        [Required]
+        public string ClientName { get; set; } = "";
+        public string IdentityTokenLifetime { get; set; } = "";
+        public string AccessTokenLifetime { get; set; } = "";
+        public string AllowedGrantTypes { get; set; } = "";
+        public string ClientSecrets { get; set; } = "";
+        public string AllowedScopes { get; set; } = "";
+        public string AllowedCorsOrigins { get; set; } = "";
+        public string RedirectUris { get; set; } = "";
+        public string PostLogoutRedirectUris { get; set; } = "";
+        public bool RequireConsent { get; set; } = false;
+        public bool AllowRememberConsent { get; set; } = false;
+        public bool AllowOfflineAccess { get; set; } = false;
+        public bool AllowAccessTokensViaBrowser { get; set; } = false;
     }
 
     public class ConfigControllerLogic
     {
-        public IdentityServer4.EntityFramework.Entities.Client GetClientResource(string clientId)
+        public ClientViewModel GetClientResource(string clientId)
         {
-            IdentityServer4.EntityFramework.Entities.Client clientResource = new IdentityServer4.EntityFramework.Entities.Client();
+            ClientViewModel clientResource = new ClientViewModel();
 
-            var tmpClientResource = GetClientResources().FirstOrDefault(x => x.ClientId == clientId);
-            if (tmpClientResource != null)
+            var data = GetClientResources().FirstOrDefault(x => x.ClientId == clientId);
+            if (data != null)
             {
-                clientResource = tmpClientResource;
+                clientResource = new ClientViewModel()
+                {
+                    ClientId = data.ClientId,
+                    ClientName = data.ClientName,
+                    IdentityTokenLifetime = data.IdentityTokenLifetime.ToString(),
+                    AccessTokenLifetime = data.AccessTokenLifetime.ToString(),
+                    AllowedGrantTypes = string.Join(Environment.NewLine, data.AllowedGrantTypes.Select(x => x.GrantType)),
+                    ClientSecrets = string.Join(Environment.NewLine, data.ClientSecrets.Select(x => x.Value)),
+                    AllowedScopes = string.Join(Environment.NewLine, data.AllowedScopes.Select(x => x.Scope)),
+                    AllowedCorsOrigins = string.Join(Environment.NewLine, data.AllowedCorsOrigins.Select(x => x.Origin)),
+                    RedirectUris = string.Join(Environment.NewLine, data.RedirectUris.Select(x => x.RedirectUri)),
+                    PostLogoutRedirectUris = string.Join(Environment.NewLine, data.PostLogoutRedirectUris.Select(x => x.PostLogoutRedirectUri)),
+                    RequireConsent = data.RequireConsent,
+                    AllowRememberConsent = data.AllowRememberConsent,
+                    AllowOfflineAccess = data.AllowOfflineAccess,
+                    AllowAccessTokensViaBrowser = data.AllowAccessTokensViaBrowser
+                };
             }
 
             return clientResource;
@@ -75,12 +140,19 @@ namespace SAEON.Identity.Service.Config
                 {
                     try
                     {
-                        clientResources = context.Clients.OrderBy(c => c.ClientName).ToList();
+                        clientResources = context.Clients
+                            .Include(c => c.AllowedGrantTypes)
+                            .Include(c => c.ClientSecrets)
+                            .Include(c => c.AllowedScopes)
+                            .Include(c => c.AllowedCorsOrigins)
+                            .Include(c => c.RedirectUris)
+                            .Include(c => c.PostLogoutRedirectUris)
+                            .OrderBy(c => c.ClientId).ToList();
                     }
                     catch (Exception ex)
                     {
                         var logger = serviceProvider.GetRequiredService<ILogger<Program>>();
-                        logger.LogError(ex, "Unabled to get ClientResource from DB.");
+                        logger.LogError(ex, "Unabled to get ClientResources from DB.");
                     }
 
                 }
---
+++ SAEON.Identity.Service/Config/ConfigModels.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SAEON.Identity.Service.Config
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public List<string> Roles { get; set; } = new List<string>();
+    }
+
+    public class Scope
+    {
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+    }
+
+    public class API
+    {
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+        public string Description { get; set; }
+        public List<string> Claims { get; set; } = new List<string>();
+        public List<string> Secrets { get; set; } = new List<string>();
+        public List<Scope> Scopes { get; set; } = new List<Scope>();
+    }
+
+    public class Client
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string GrantType { get; set; }
+        public bool RequireConsent { get; set; }
+        public bool RememberConsent { get; set; }
+        public bool OfflineAccess { get; set; }
+        public bool AccessTokensViaBrowser { get; set; }
+        public int IdentityTokenLifetime { get; set; } = (int)new TimeSpan(14, 0, 0).TotalSeconds;
+        public int AccessTokenLifetime { get; set; } = (int)new TimeSpan(7, 0, 0).TotalSeconds;
+        public List<string> Secrets { get; set; } = new List<string>();
+        public List<string> Scopes { get; set; } = new List<string>();
+        public List<string> CorsOrigins { get; set; } = new List<string>();
+        public List<string> RedirectURIs { get; set; } = new List<string>();
+        public List<string> PostLogoutRedirectURIs { get; set; } = new List<string>();
+    }
+}
