221dd83b49705668faf04f9dd25c9baf4bac0a09
andree1985 <30987911+andree1985@users.noreply.github.com>
2018-07-09 22:53:49 +0200

    Done: Delete user account #12

--- SAEON.Identity.Service/Manage/ManageController.cs
+++ SAEON.Identity.Service/Manage/ManageController.cs
@@ -489,6 +489,62 @@ namespace SAEON.Identity.Service.UI
             return View(user);
         }
 
+        [HttpGet]
+        public async Task<IActionResult> UserSelfDelete()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                throw new ApplicationException($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            // Validate business rules to ensure self-deletion is allowed, though it would
+            // be a good idea to tell the user why their account cannot be deleted
+            var userSelfDelete = new SAEONUser
+            {
+                FirstName = user.FirstName,
+                Surname = user.Surname,
+                Email = user.Email,
+                UserName = user.UserName
+            };
+
+            return View(userSelfDelete);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> UserSelfDelete(SAEONUser model)
+        {
+            if(model.PasswordHash == null)
+            {
+                model.PasswordHash = "";
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                throw new ApplicationException($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            if (await _userManager.CheckPasswordAsync(user, model.PasswordHash) == false)
+            {
+                ModelState.AddModelError("PasswordHash", "Incorrect password.");
+                return View(model);
+            }
+
+            await _signInManager.SignOutAsync();
+            _logger.LogInformation("User logged out prior to account deletion.");
+
+            await _userManager.DeleteAsync(user);
+
+            return RedirectToAction(nameof(HomeController.Index), "Home");
+        }
+
         #region Helpers
 
         private void AddErrors(IdentityResult result)
