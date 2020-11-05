fbaa36ebf434b6b15b261e5ba61595419353d06f
Tim Parker-Nance (SAEON) <timpn@saeon.ac.za>
2020-11-05 12:30:40 +0200

    SameSite=None #26

--- SAEON.Identity.Service/Startup.cs
+++ SAEON.Identity.Service/Startup.cs
@@ -40,6 +40,7 @@ namespace SAEON.Identity.Service
                     options.CheckConsentNeeded = context => true;
                     options.MinimumSameSitePolicy = SameSiteMode.None;
                 });
+                services.AddAntiforgery(options => { options.Cookie.Expiration = TimeSpan.Zero; });
                 //services.AddAntiforgery(options =>
                 //{
                 //    options.Cookie.SameSite = SameSiteMode.None;
